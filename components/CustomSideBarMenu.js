736fe53df7872fcaa4450c847541f826a882768f
Abijeethashok <61748089+Abijeethashok@users.noreply.github.com>
2021-02-27 20:53:57 +0530

    Update CustomSideBarMenu.js

--- components/CustomSideBarMenu.js
+++ components/CustomSideBarMenu.js
@@ -1,13 +1,52 @@
 import React, { Component} from 'react';
 import {StyleSheet, View, Text,TouchableOpacity} from 'react-native';
 import { DrawerItems} from 'react-navigation-drawer'
+import {Avatar} from 'react-native-elements'
+import * as ImagePicker from 'expo-image-picker'
+import * as Permisions from 'expo-Permisions'
+import db from '../config'
 
 import firebase from 'firebase';
 
 export default class CustomSideBarMenu extends Component{
+  constructor(){
+    super()
+   this.state = {
+     userId : firebase.auth().currrentUser.email,
+     image :  null,
+     name : "",
+     docId : ""
+
+   }
+  }
+  selectpicture = async ()=>{
+    const{cancled,uri}  = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes : ImagePicker.MediaTypesOptions.All,allowsEditing : true,aspect : [4,3],quality : 1 
+    }) 
+    if(!cancled){this.uploadpicture(uri)}
+   }
+
+   uploadpicture= (uri)=>{db.collection('users').doc(this.state.docId).update({image:uri})}
+   
+   getuserdetials(){
+     db.collection('users').where('email_id','==',this.state.userId).onSnapshot(query=>{
+       query.forEach(doc=>{
+         this.setState({
+           name : doc.data().first_name,docId : doc.id , image : doc.data().image
+         })
+       })
+     })
+   }
+
+   componentDidMount(){
+     this.getuserdetials()
+   }
+
   render(){
     return(
       <View style={{flex:1}}>
+        
+
         <View style={styles.drawerItemsContainer}>
           <DrawerItems {...this.props}/>
         </View>
@@ -19,6 +58,10 @@ export default class CustomSideBarMenu extends Component{
           }}>
             <Text>Log Out</Text>
           </TouchableOpacity>
+
+          <Avatar rounded icon = {{name : 'user', type : 'font-awesome'}}
+        source = {{uri : this.state.image}} size = "medium" containerStyle = {{flex : 0.75 , width : "40%" ,height : "20%",marginLeft : 20 ,marginTop : 30,borderRadius:40 }} onPress = {()=>this.selectpicture()}/>
+
         </View>
       </View>
     )
